6d22c382919ff1a8ad976131c9e924a8212a4fbd
anu <Anuroyw12@gmail.com>
2025-01-28 22:02:58 +0530

    dailyQuoteApp

---
+++ app/src/main/java/com/example/dailyquote/Screen2.java
@@ -0,0 +1,38 @@
+package com.example.dailyquote;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Screen2 extends AppCompatActivity {
+Button button2;
+TextView quote2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_screen2);
+        button2 = findViewById(R.id.button2);
+        quote2 = findViewById(R.id.quote2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(Screen2.this,MainActivity.class);
+                startActivity(intent2);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
